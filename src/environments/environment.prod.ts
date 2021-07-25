007ce79b5e34ce415440d4265c3952d9f040fbb6
muhammadali1995 <4377433mtDEV>
2021-07-25 19:32:14 +0500

    small fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  Api: {
+  api: {
     charts: 'https://data.cdc.gov/resource/saz5-9hgg.json',
     tables: 'https://soda.demo.socrata.com/resource/6yvf-kk3n.json'
   }
